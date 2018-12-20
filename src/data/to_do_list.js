30da21fbf88bb71674a458076f0c790a0d419db7
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-19 16:32:39 -0800

    completed displaying list items with dummy data

---
+++ src/data/to_do_list.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        _id: '001',
+        title: 'Wash Car',
+        details: 'Wash the blue car',
+        complete: false
+    },
+    {
+        _id: '002',
+        title: 'Crash Car',
+        details: 'Crash the blue car into the red car',
+        complete: false
+    },
+    {
+        _id: '003',
+        title: 'Repair Car',
+        details: 'Repair the blue car',
+        complete: false
+    },
+    {
+        _id: '004',
+        title: 'Call Insurance',
+        details: 'Call Survival now',
+        complete: false
+    },
+    {
+        _id: '005',
+        title: 'Repair Car',
+        details: 'Repair the red car',
+        complete: false
+    }
+]
